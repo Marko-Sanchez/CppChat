801a4bfced1eeabdee3498ad5a73ee741536ebbf
Marko <sanchezmarco0909@gmail.com>
2022-04-21 23:10:07 -0700

    Server - Client connection:
    - server and client are able to send messages back-and-fourth to each other.
    
    - synchronous multiplexing to handle many client connections.
    
    - client reads input from terminal and outputs messages from server.

---
+++ client.cpp
@@ -0,0 +1,178 @@
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <cstdio>
+#include <future>
+
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+constexpr uint16_t PORT{8080};
+constexpr ssize_t BUFFER_SIZE{1024};
+/* Contianer for colored text values to output onto terminal */
+const struct Colors{
+
+    const std::string pass{"\033[0;32m"};
+    const std::string sMsg{"\033[0;36m"};
+    const std::string warning{"\033[0;33m"};
+    const std::string error{"\033[0;31m"};
+
+    const std::string end{"\033[0m"};
+
+}color;
+
+/*
+ * function to be passed to std::async(), to be processed asynchronously.
+ * Reads user input from terminal, then sends message to server.
+ * @params:     serverFD{const int} file descriptor to write to server.
+ *              pfdWrite{const int} file descriptor to alert self to close.
+ *
+ * @output: sends message to server.
+ */
+void clientInput(const int serverFD, const int pfdWrite)
+{
+    char buffer[BUFFER_SIZE] = {0};
+    int flags{0};
+    ssize_t bytes{0};
+
+    std::cout << "> ";
+
+    while(std::cin.getline(buffer, BUFFER_SIZE))
+    {
+
+        // write message to server:
+        auto length = static_cast<ssize_t>(strlen(buffer));
+        bytes = send(serverFD, buffer, length, flags);
+
+        if(bytes <= 0)
+        {
+            write(pfdWrite, buffer, 0); // write to self, to close connection:
+            close(pfdWrite);            // close write end of pipe:
+            break;
+        }
+
+        std::cout << "> ";
+    }
+
+}
+
+/*
+ * listens for activity on server and pipe file descriptors. If server
+ * outputs message onto terminal, else begins shutting down process.
+ * Launches thread to listen for user input on terminal.
+ * @params:     serverFD{const int} file descriptor to write to server.
+ *
+ * @output: message from server onto terminal.
+ */
+void chat(const int serverFD)
+{
+
+    char buffer[BUFFER_SIZE] = {0};
+    int flags{0};
+    ssize_t bytes{0};
+
+    // output server response:
+    bytes = recv(serverFD,  &buffer, sizeof(buffer), flags);
+    std::cout << color.sMsg << buffer << color.end << std::endl;
+
+    fd_set socketset;
+    pid_t childpid;
+    int pipefd[2];
+
+    // establish pipe to write to self:
+    if(pipe(pipefd) == -1)
+    {
+        std::cerr << color.error << "error establishing pipe-to-self" << color.end << std::endl;
+
+        close(serverFD);
+        return;
+    }
+
+    // thread for processsing user input:
+    auto ft = std::async(std::launch::async, clientInput, serverFD, pipefd[1]);
+
+    int nfds{std::max(serverFD, pipefd[0]) + 1};
+
+    while(true)
+    {
+
+        FD_ZERO(&socketset);
+        FD_SET(serverFD, &socketset);
+        FD_SET(pipefd[0], &socketset);
+
+
+        // listen for activity from server and self:
+        int ready{select(nfds, &socketset, nullptr, nullptr, nullptr)};
+
+        if(ready <= 0)
+        {
+            std::cerr << color.error << "server error" << color.end << std::endl;
+            break;
+        }
+        else if(FD_ISSET(pipefd[0], &socketset))
+        {
+            send(serverFD, buffer, 0, flags);// send 0 bytes to server, indicating closure:
+
+            std::cout << color.warning << "closing connection to server" << color.end << std::endl;
+            break;
+        }
+
+        // output server response, in a child process:
+        if((childpid = fork()) == 0)
+        {
+            bytes = recv(serverFD, &buffer, BUFFER_SIZE, flags);
+            std::cout << "Server: " << buffer << std::endl;
+
+            std::cout << "> ";
+            std::flush(std::cout);
+
+            close(serverFD);
+            exit (0);
+        }
+
+        memset(buffer, 0, bytes);
+    }
+
+    close(pipefd[0]);        // close read end of pipe:
+    close(pipefd[1]);        // close write end of pipe:
+    close(serverFD);         // close socket to server:
+}
+
+int main(int argc, char* argv[])
+{
+
+    /* Setting up socket and connection to server */
+
+    struct sockaddr_in serverAddr;
+
+    int server_fd{socket(AF_INET, SOCK_STREAM, 0)};
+
+    if(server_fd < 0)
+    {
+        std::cerr << color.error << "socket connection failed" << color.end << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::cout << color.pass << "socket connected" << color.end << std::endl;
+
+    // setup sockets method of connection:
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(PORT);
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    // Connect to server: Can use this same method to connect to another client, once serversneds back client info for us
+    if(connect(server_fd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)))
+    {
+        std::cerr << color.error << "connection to server failed" << color.end << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::cout << color.pass << "connected to server" << color.end << std::endl;
+
+    chat(server_fd);
+
+    return EXIT_SUCCESS;
+}
---
+++ server.cpp
@@ -0,0 +1,198 @@
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+
+constexpr uint16_t PORT{8080};
+constexpr ssize_t BUFFER_SIZE{1024};
+
+/* Contianer for colored text values to output onto terminal */
+const struct Colors{
+
+    const std::string pass{"\033[0;32m"};
+    const std::string sMsg{"\033[0;36m"};
+    const std::string warning{"\033[0;33m"};
+    const std::string error{"\033[0;31m"};
+
+    const std::string end{"\033[0m"};
+
+}color;
+
+int main(int argc, char* argv[])
+{
+    /* setting up socket information */
+
+    struct sockaddr_in address;
+
+    // Connect using: domain: IPV4, comunication tye: TCP, and using internet protocol (0)
+    int server_fd{socket(AF_INET, SOCK_STREAM, 0)};
+
+    if(server_fd == 0)
+    {
+        std::cerr << color.error << "socket connection failed" << color.end << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::cout << color.pass << "socket connected" << color.end << std::endl;
+
+    // Attach socket to port:
+    int option{1};
+    if(setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &option, sizeof(option)))
+    {
+        std::cerr << color.error << "error in setting socket options" << color.end << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    // INADDR_ANY: bind socket to all local interfaces, using IPV4 family:
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if(bind(server_fd, (struct sockaddr*)&address, sizeof(address)))
+    {
+        std::cerr << color.error << "binding socket failed" << color.end << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    if(listen(server_fd, 5) < 0)
+    {
+        std::cerr << color.error << "unable to start listening" << color.end << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::cout << color.pass << "listening..." << color.end << std::endl;
+
+    /* server starts listening and processing client connections */
+
+    // client address container:
+    struct sockaddr_in client;
+    socklen_t len{sizeof(client)};
+
+    // monitor multiple file descriptors:
+    fd_set socketset;
+    int max_connections{6};
+
+    // client file descriptor container:
+    std::vector<int> clients(max_connections, 0);
+    int currentConnections{0};
+
+    char welcomeMsg[] = "Welcome to the Server!";
+    char response[] = "Message received\n";
+    char buffer[BUFFER_SIZE] = {0};
+
+    int testCounter{1};
+    while(true)
+    {
+
+        FD_ZERO(&socketset);
+        FD_SET(server_fd, &socketset);
+
+        // max file descriptor for select listen range:
+        int maxFD{server_fd};
+
+        // add all client sockets back to set:
+        for(const auto& cli: clients)
+        {
+            if(cli != 0)
+            {
+                FD_SET(cli, &socketset);
+                std::cout << cli << std::endl;
+                maxFD = std::max(maxFD, cli);
+            }
+
+        }
+
+        // listen for activity on all sockets in set range:
+        int ready{select(maxFD + 1, &socketset, nullptr, nullptr, nullptr)};
+
+        // check if activity was caused by server or client:
+        if(FD_ISSET(server_fd, &socketset))
+        {
+
+            // if we have reached maxed connections do not accept:
+            if(currentConnections == max_connections)
+            {
+                std::cout << color.warning << "max connections reached" << color.end << std::endl;
+                continue;
+            }
+
+            int incomingClient{accept(server_fd, (struct sockaddr *)&client, (socklen_t *)&len)};
+            if(incomingClient < 0)
+            {
+                std::cerr << color.error << "unable to accept to client" << color.end << std::endl;
+                continue;
+            }
+
+            std::cout << color.warning << "new connection, socket: " << incomingClient
+                      << " ,there IP is: " << inet_ntoa(client.sin_addr) << color.end
+                      << std::endl;
+
+            ssize_t bytes = send(incomingClient, welcomeMsg, sizeof(welcomeMsg), 0);
+
+            // add new client to set and container:
+            FD_SET(incomingClient, &socketset);
+
+            auto iter = find(begin(clients), end(clients), 0);
+            *iter = incomingClient;
+
+            currentConnections++;
+
+        }else
+        {
+
+            for(auto & client: clients)
+            {
+                if(FD_ISSET( client, &socketset))
+                {
+                    ssize_t bytes = recv(client,  buffer, sizeof(buffer), 0);
+                    std::cout << "number of bytes read: " << bytes << std::endl;
+
+                    // dicsonect if no bytes were sent, else output message to terminal:
+                    if(bytes <= 0)
+                    {
+                        std::cout << color.warning << "client no longer wants to talk" << color.end << std::endl;
+
+                        // close client connection and remove from container:
+                        close(client);
+                        client = 0;
+                        currentConnections--;
+
+                    }else
+                    {
+                        // output client response:
+                        std::cout << "client: " << buffer << std::endl;
+                        testCounter++;
+
+                        // welcome new client to the server: pusedo reply testing
+                        if(testCounter == 3)
+                        {
+                            bytes = send(client, welcomeMsg, sizeof(welcomeMsg), 0);
+                            std::cout << "Msg sent: " << bytes << std::endl;
+                        }
+                        testCounter = testCounter % 3;
+
+                    }
+
+                    // clear buffer:
+                    memset(buffer, 0, BUFFER_SIZE);
+
+                }
+            }
+        }
+
+        // no clients connected, stop listening:
+        if(currentConnections == 0)break;
+    }
+
+    close(server_fd);
+
+    return EXIT_SUCCESS;
+}
