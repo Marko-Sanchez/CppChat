6ddc5abaf482b74a02d3f66b10e07c804ef69f10
Marko A. Sanchez <sanchezmarco0909@gmail.com>
2023-05-13 20:07:54 -0700

    Added additional comments

--- server/server.hpp
+++ server/server.hpp
@@ -13,6 +13,9 @@
 
 using namespace boost;
 
+/*
+ * Handles the process of listening for new incoming connections.
+ */
 class Acceptor
 {
     private:
@@ -22,6 +25,13 @@ class Acceptor
 
         std::shared_ptr<Database> m_database;
 
+        /*
+         * Listens for client connections. Creates a socket and once client connects associates
+         * socket with client.
+         *
+         * behavior:
+         *          calls onAccept() to process new client connection.
+         */
         void initAccept()
         {
             auto sock = std::make_shared<asio::ip::tcp::socket>(asio::ip::tcp::socket(m_ios));
@@ -35,7 +45,7 @@ class Acceptor
                         }
                         else
                         {
-                            std::cout << "Connection to client succesfull..." << std::endl;
+                            std::cout << "Connection to client successful..." << std::endl;
                             onAccept(ec, sock);
                         }
                     });
@@ -74,12 +84,27 @@ class Acceptor
 
     public:
 
+        /*
+         * Constructor binds asio::acceptor with port to listen on.
+         *
+         * parameters:
+         *              ios, object representing machine network resources.
+         *              port_num, port to listen on.
+         * behavior:
+         *          initializes io_service object and binds asio::acceptor object to port and system resources.
+         */
         Acceptor(asio::io_service &ios, unsigned short port_num):
             m_ios(ios),
             m_acceptor(m_ios, asio::ip::tcp::endpoint(asio::ip::address_v4::any(), port_num)),
             m_isStopped(false)
         {}
 
+        /*
+         * Starts accepting connections.
+         *
+         * behavior:
+         *          sets acceptor to handle 30 connections at once, ignores rest.
+         */
         void start()
         {
             std::cout << "Starting server..." << std::endl;
@@ -109,18 +134,22 @@ class AsyncTCPServer
 
     public:
 
+        /* Constructor. */
         AsyncTCPServer()
         {
             m_work.reset(new asio::io_service::work(m_ios));
         }
 
         /*
+         * Starts accepting connections and creates worker threads to handle request.
+         *
          * param:
          *      port_num port on which server will listen on.
          *      thread_pool_size how many threads to create.
          *
-         * behavior:
-         *      starts accepting connections and creates worker threads to handle request.
+         *  behavior:
+         *      calls Acceptor.start() which will invoke a asynchronous function that will listen
+         *      for connections.
          */
         void start(unsigned short port_num, std::size_t thread_pool_size)
         {
@@ -133,13 +162,18 @@ class AsyncTCPServer
 
             for(unsigned int i{0}; i < thread_pool_size; ++i)
             {
-                /* auto process = std::make_unique<std::thread>(std::thread([this](){m_ios.run();})); */
                 auto process = std::make_unique<std::thread>([this](){m_ios.run();});
 
                 m_threadpool.push_back(std::move(process));
             }
         }
 
+        /*
+         * Stops server and joins threads.
+         *
+         * behavior:
+         *          calls Acceptor to stop, stops io_service object, and joins threads.
+         */
         void stop()
         {
 
